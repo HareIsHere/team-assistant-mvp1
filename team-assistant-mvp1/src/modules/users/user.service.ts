20cf6f15c22b7960804d25b3bb3780cb8e8c4aeb
Hare <106960377+HareIsHere@users.noreply.github.com>
2026-09-08 16:46:59 +0700

    Delete team-assistant-mvp1 directory

--- team-assistant-mvp1/src/modules/users/user.service.ts
+++
@@ -1,9 +0,0 @@
-import { prisma } from "../../lib/prisma.js";
-
-export async function findOrCreateUser(whatsappId: string, name?: string) {
-  return prisma.user.upsert({
-    where: { whatsappId },
-    update: name ? { name } : {},
-    create: { whatsappId, name }
-  });
-}
