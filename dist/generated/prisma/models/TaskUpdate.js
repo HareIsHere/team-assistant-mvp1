6c6bed0450ed7274ab024124fa7afc08d0627bbd
Hare <106960377+HareIsHere@users.noreply.github.com>
2026-09-08 20:04:50 +0700

    Delete dist directory

--- dist/generated/prisma/models/TaskUpdate.js
+++
@@ -1,2 +0,0 @@
-export {};
-//# sourceMappingURL=TaskUpdate.js.map
--- dist/src/modules/users/user.service.js
+++
@@ -1,9 +0,0 @@
-import { prisma } from "../../lib/prisma.js";
-export async function findOrCreateUser(whatsappId, name) {
-    return prisma.user.upsert({
-        where: { whatsappId },
-        update: name ? { name } : {},
-        create: { whatsappId, name }
-    });
-}
-//# sourceMappingURL=user.service.js.map
